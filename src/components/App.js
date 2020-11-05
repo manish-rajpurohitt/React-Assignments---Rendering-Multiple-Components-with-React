918bd7746ee77ae504e138f709569cfaa5a5be83
Manish Purohit <manishkumar9866@gmail.com>
2020-11-05 22:42:29 +0530

    fsdb

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">Project-1 Title</h1>
+      <p data-ns-test="project-description">project-1 Description</p>
+      <h1 data-ns-test="project-name">Project-2 Title</h1>
+      <p data-ns-test="project-description">project-2 Description</p>
+    </>
+  );
+};
 
 export default App;
